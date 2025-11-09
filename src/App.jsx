cd840775cc36c8f33c8f617fd60b7fb2aa1cb2f9
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-11-09 17:13:08 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+//import React from "react";
+import ButtonGradient from './assets /svg/ButtonGradient';
+import Button from "./components/Button";
+import Header from './components/Header';
+
 
 const App = () => {
   return (
-    <div>
-      
-      App
-      
-    </div>
-  )
-}
+    <>
 
-export default App
+      <div className="pt-19 lg:pt-21  overflow-hidden ">
+        <Header />
+
+
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
